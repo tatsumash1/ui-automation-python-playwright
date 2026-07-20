da2ee045c750635b80f74da9c8c193a982cd343c
Aleksandr Mironov <mironovs.2004@gmail.com>
2026-07-20 13:27:23 +0200

    Добавил чек-лист, тест-кейсы для логина и тесты для заблокированного пользователя

--- tests/test_login.py
+++ tests/test_login.py
@@ -17,3 +17,12 @@ def test_unsuccessful_login(page):
 
     expect(page.locator(".error-message-container")).to_be_visible()
     expect(page.locator(".error-message-container")).to_have_text("Epic sadface: Username and password do not match any user in this service")
+
+def test_blocked_user_login(page):
+    login_page = LoginPage(page)
+
+    login_page.open()
+    login_page.login("locked_out_user", "secret_sauce")
+
+    expect(page.locator(".error-message-container")).to_be_visible()
+    expect(page.locator(".error-message-container")).to_have_text("Epic sadface: Sorry, this user has been locked out.")
