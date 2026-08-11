fa3c762011dbe8f5fa2e1651e0c7f52d326c1d11
Aleksandr Mironov <mironovs.2004@gmail.com>
2026-08-11 18:14:02 +0200

    Написаны 3 фикстуры logged_in_page, product_page, clean_cart

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import pytest
+
+#Логин
+@pytest.fixture(scope="function")
+def logged_in_page(page):
+    page.goto("https://saucedemo.com/")
+    page.locator("#username").fill("standard_user")
+    page.locator("#password").fill("secret_sauce")
+    page.locator("#login-button").click()
+
+    return page
+
+# Переход на страницу продуктов (связанная с фикстурой logged_in_page)
+@pytest.fixture(scope="function")
+def products_page(logged_in_page):
+    page = logged_in_page
+
+    assert page.locator(".inventory_list").is_visible(), "PRODUCTS PAGE NOT VISIBLE"
+
+    return page
+
+#Очистка корзины (связанная с фикстурой products_page)
+@pytest.fixture
+def clean_cart(products_page):
+
+    page = products_page
+
+    page.locator(".add-to-cart-sauce-labs-backpack").click()
+
+    assert page.locator(".shopping_cart_badge") .text_content() == "1", "CART IS NOT UPDATED"
+
+    yield products_page
+
+    remove_buttons = page.locator('[data-test^="remove-"]')
+
+    for i in range(remove_buttons.count()):
+        remove_buttons.nth(0).click()
+
+        
