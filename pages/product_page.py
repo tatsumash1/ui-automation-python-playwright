34184e6127ca556f9904b206f0abea17c403d654
Aleksandr Mironov <mironovs.2004@gmail.com>
2026-08-28 00:36:28 +0200

    Написаны функции для взаимодействия с конкретными товарами

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,7 +9,21 @@ class ProductPage:
         """Фильтрация картрочек по нужному имени"""
         return self.inventory_items.filter(has_text(product_name))
 
-    #def open_product_details(self, product_name: str):
+    def open_product_details(self, product_name: str):
+        """Открытие карточки товара путем нажатия на название"""
+        container = self.get_product_container(product_name)
+        container.locator(".inventory_item_name").click()
+
+    def add_to_cart(self, product_name: str):
+        """Добавление конкретного товара в корзину"""
+        container = self.get_product_container(product_name)
+        container.locator(".add-to-cart").click()
+
+    def get_product_price(self, product_name: str):
+        """Получение цены конкретного товара"""
+        container = self.get_product_container(product_name)
+        price_text = container.locator(".inventory_item_price").text_content()
+        return price_text
 
     
 
