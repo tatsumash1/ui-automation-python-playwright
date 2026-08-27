3a404d016080f061baaaa5147fe42386c6d22494
Aleksandr Mironov <mironovs.2004@gmail.com>
2026-08-28 01:28:20 +0200

    Выдает ошибку на 5 карточках - AssertionError: Locator expected to have text '1'
    последний тест с 6 карточкой уходит в таймаут

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,28 +1,39 @@
-from playwright.sync_api import Page, locator, has_text
+from playwright.sync_api import Locator
 
 class ProductPage:
     def __init__(self, page):
         self.page = page
         self.inventory_items = page.locator(".inventory_item")
+        self.add_to_cart_button = page.locator("button[id^='add-to-cart']")
+        self.remove_button = page.locator("button[id^='remove']")
+        self.shopping_cart_button = page.locator("[data-test='shopping_cart_link']")
+        self.cart_badge = page.locator("[data-test='shopping_cart_badge']")
+        self.inventory_item_name = page.locator(".inventory_item_name")
+        self.inventory_item_price = page.locator(".inventory_item_price")
 
-    def get_product_container(self, product_name: str)-> locator: 
+    def get_product_container(self, product_name: str)-> Locator: 
         """Фильтрация картрочек по нужному имени"""
-        return self.inventory_items.filter(has_text(product_name))
+        return self.inventory_items.filter(has_text =product_name)
 
     def open_product_details(self, product_name: str):
         """Открытие карточки товара путем нажатия на название"""
         container = self.get_product_container(product_name)
-        container.locator(".inventory_item_name").click()
+        container.locator(self.inventory_item_name).click()
 
     def add_to_cart(self, product_name: str):
         """Добавление конкретного товара в корзину"""
         container = self.get_product_container(product_name)
-        container.locator(".add-to-cart").click()
+        container.locator(self.add_to_cart_button).click()
+
+    def remove_from_cart(self, product_name: str):
+        """Удаление конкретного товара из корзины (сброс состояния)"""
+        container = self.get_product_container(product_name)
+        container.locator(self.remove_button).click()
 
     def get_product_price(self, product_name: str):
         """Получение цены конкретного товара"""
         container = self.get_product_container(product_name)
-        price_text = container.locator(".inventory_item_price").text_content()
+        price_text = container.locator(self.inventory_item_price).inner_text()
         return price_text
 
     
---
+++ tests/test_products.py
@@ -0,0 +1,27 @@
+from pages.product_page import ProductPage
+from playwright.sync_api import expect
+import pytest
+
+
+PRODUCTS_DATA=[
+    {"name": "Sauce Labs Backpack", "price": "29.99"},
+    {"name": "Sauce Labs Bike Light", "price": "9.99"},
+    {"name": "Sauce Labs Bolt T-Shirt", "price": "15.99"},
+    {"name": "Sauce Labs Fleece Jacket", "price": "49.99"},
+    {"name": "Sauce Labs Onesie", "price": "7.99"},
+    {"name": "Test.allTheTheThings() T-Shirt (Red)", "price": "15.99"}
+]
+
+@pytest.mark.parametrize("product", PRODUCTS_DATA, ids=lambda product: product["name"])
+def test_product_card(logged_in_page_valid, product: dict[str, str]):
+    product_page = ProductPage(logged_in_page_valid)
+    actual_price = product_page.get_product_price(product["name"]).strip("$")
+    assert actual_price == product["price"], f"Ожидаемая цена {product['price']}, фактическая {actual_price}"
+
+    product_page.add_to_cart(product["name"])
+
+    expect(product_page.cart_badge).to_have_text("1")
+
+    product_page.remove_from_cart(product["name"])
+
+    expect(product_page.cart_badge).to_be_hidden()
